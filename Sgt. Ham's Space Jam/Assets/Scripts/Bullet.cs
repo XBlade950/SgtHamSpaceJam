1ecf41d40015e31b55f0cb2ea7070bc23c81bbcd
XBlade950 <alexander.ledet95@yahoo.com>
2015-11-22 00:07:21 -0600

    Adding Game to Repo

---
+++ Sgt. Ham's Space Jam/Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += transform.forward;
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Bullet hit something");
+        Destroy(gameObject);
+        //Destroy(this);
+    }
+}
