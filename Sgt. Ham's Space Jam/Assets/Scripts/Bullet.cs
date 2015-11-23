2680ca83b42c7a64167c7ae5b94a2881144fc79b
XBlade950 <alexander.ledet95@yahoo.com>
2015-11-23 12:23:58 -0600

    fixed bullet script
    allowed you to change speed

--- Sgt. Ham's Space Jam/Assets/Scripts/Bullet.cs
+++ Sgt. Ham's Space Jam/Assets/Scripts/Bullet.cs
@@ -12,13 +12,13 @@ public class Bullet : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position += transform.forward;
+        transform.position += transform.forward*speed;
 	}
 
     void OnCollisionEnter(Collision col)
     {
         Debug.Log("Bullet hit something");
         Destroy(gameObject);
-        //Destroy(this);
+        
     }
 }
